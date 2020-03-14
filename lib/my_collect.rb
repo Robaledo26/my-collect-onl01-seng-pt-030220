5ef08fea312d406e74078d6fd6ead72808163add
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-14 00:39:17 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,22 @@
 
   
-def my_collect(arr)
-  i =  0 
-  na - []
-  while < arr.length 
+#def my_collect(arr)
+ # i =  0 
+ # na - []
+ # while < arr.length 
   na << yelid(arr[i])
-   i += 1 
- end
-  na
+ #  i += 1 
+# end
+#  na
+#end
+
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
   
